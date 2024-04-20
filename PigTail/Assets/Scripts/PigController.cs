74da21c51631a6dee0e7a6ebe5cea8f216a4402e
green-Desktop <appieosugood@gmail.com>
2024-04-20 15:30:12 +0800

    pig add scriptobject

--- PigTail/Assets/Scripts/PigController.cs
+++ PigTail/Assets/Scripts/PigController.cs
@@ -1,14 +1,12 @@
 using System;
-
+using choose;
 using UnityEngine;
 using UnityEngine.UI;
 
 
 public class PigController: MonoBehaviour
-    {
-        [SerializeField] float maxForce = 0;
-        [SerializeField] float minForce = 0;
-        [SerializeField] float tiredDecreaceSpeed = 1;
+{
+    public ChoosedPig choosedPig;
         [SerializeField] Slider tiedBar;
         [SerializeField] LevelSettings levelSettings;
 
@@ -33,13 +31,13 @@ public class PigController: MonoBehaviour
         private void Update() {
             tiedBar.value = currTiredNum/tiredNum;
             if(isStart){
-                currTiredNum = Mathf.Max(0,currTiredNum - tiredDecreaceSpeed * Time.deltaTime);
+                currTiredNum = Mathf.Max(0,currTiredNum - choosedPig.tiredDecreaceSpeed * Time.deltaTime);
                 
             }
         }
         public float GetForce(){
             float tiredSpeed = (currTiredNum/tiredNum)*levelSettings.TiredForceMod;
-            return UnityEngine.Random.Range(minForce+tiredSpeed ,maxForce+tiredSpeed);
+            return UnityEngine.Random.Range(choosedPig.minForce+tiredSpeed ,choosedPig.maxForce+tiredSpeed);
         }
         
     }
