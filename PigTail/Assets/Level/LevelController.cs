a6de06b9f6353e756350d7f521c5faeb4b7d48ce
green-Desktop <appieosugood@gmail.com>
2024-04-20 16:21:40 +0800

    hard code

--- PigTail/Assets/Level/LevelController.cs
+++ PigTail/Assets/Level/LevelController.cs
@@ -1,4 +1,6 @@
-﻿using choose;
+﻿using System;
+using System.Collections.Generic;
+using choose;
 using UnityEngine;
 
 namespace Level
@@ -6,6 +8,19 @@ namespace Level
     public class LevelController:MonoBehaviour
     {
         public PlayerLevel playerLevel;
+        public List<List<float>> levelData;
+
+        private void Awake()
+        {
+            levelData = new List<List<float>>()
+            {
+                new List<float>(){10,5,1},
+                new List<float>(){20,10,2},
+                new List<float>(){30,10,3},
+                new List<float>(){40,20,4},
+                new List<float>(){50,20,5},
+            };
+        }
 
         void Win()
         {
@@ -20,10 +35,11 @@ namespace Level
         public ChoosedPig GetNowLevelPig()
         {
              var cp = ScriptableObject.CreateInstance<ChoosedPig>();
+             var data=levelData[playerLevel.level % levelData.Count];
              cp.pigType= PigType.Pig1;
-             cp.maxForce = 0;
-             cp.minForce = 1 ;
-             cp.tiredDecreaceSpeed = 2;
+             cp.maxForce = data[0] *((float)playerLevel.level / levelData.Count);
+             cp.minForce = data[1] *((float)playerLevel.level / levelData.Count);
+             cp.tiredDecreaceSpeed = data[2] *((float)playerLevel.level / levelData.Count);
              return cp;
         }
     }
