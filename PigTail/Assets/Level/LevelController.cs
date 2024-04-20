575321bd030bc019c88ce7866036aeefbcd70408
green-Desktop <appieosugood@gmail.com>
2024-04-20 16:11:05 +0800

    level manager

---
+++ PigTail/Assets/Level/LevelController.cs
@@ -0,0 +1,30 @@
+﻿using choose;
+using UnityEngine;
+
+namespace Level
+{
+    public class LevelController:MonoBehaviour
+    {
+        public PlayerLevel playerLevel;
+
+        void Win()
+        {
+            playerLevel.level++;
+        }
+
+        void Lose()
+        {
+            playerLevel.level = 0;
+        }
+
+        public ChoosedPig GetNowLevelPig()
+        {
+             var cp = ScriptableObject.CreateInstance<ChoosedPig>();
+             cp.pigType= PigType.Pig1;
+             cp.maxForce = 0;
+             cp.minForce = 1 ;
+             cp.tiredDecreaceSpeed = 2;
+             return cp;
+        }
+    }
+}
--- PigTail/Assets/Scripts/GameManager.cs
+++ PigTail/Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Level;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
