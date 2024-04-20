b5b7bb434afef9b65fc85a1f908a6822d6b1ecfd
green-Desktop <appieosugood@gmail.com>
2024-04-20 16:52:10 +0800

    fix choose problem

--- PigTail/Assets/choose/ChooseController.cs
+++ PigTail/Assets/choose/ChooseController.cs
@@ -13,9 +13,9 @@ public class ChooseController : MonoBehaviour
     private GameObject pig1;
     private GameObject pig2;
     private GameObject pig3;
-    private Button pig1Button;
-    private Button pig2Button;
-    private Button pig3Button;
+    private Image pig1Image;
+    private Image pig2Image;
+    private Image pig3Image;
     
         
 
@@ -24,19 +24,31 @@ public class ChooseController : MonoBehaviour
         pig1 = GameObject.Find("Pig1");
         pig2 = GameObject.Find("Pig2");
         pig3 = GameObject.Find("Pig3");
-        pig1Button = pig1.GetComponent<Button>();
-        pig2Button = pig2.GetComponent<Button>();
-        pig3Button = pig3.GetComponent<Button>();
+        pig1Image = pig1.GetComponent<Image>();
+        pig2Image = pig2.GetComponent<Image>();
+        pig3Image = pig3.GetComponent<Image>();
+        
+        UpdateChooseColor();
+    }
+
+    private void UpdateChooseColor()
+    {
         switch (choosedPig.pigType)
         {
             case PigType.Pig1:
-                pig1Button.Select();
+                pig1Image.color = new Color(1f, 1f, 1f, 1f);
+                pig2Image.color = new Color(0.7f, 0.7f, 0.7f, 1f);
+                pig3Image.color = new Color(0.7f, 0.7f, 0.7f, 1f);
                 break;
             case PigType.Pig2:
-                pig2Button.Select();
+                pig1Image.color = new Color(0.7f, 0.7f, 0.7f, 1f);
+                pig2Image.color = new Color(1f, 1f, 1f, 1f);
+                pig3Image.color = new Color(0.7f, 0.7f, 0.7f, 1f);
                 break;
             case PigType.Pig3:
-                pig3Button.Select();
+                pig1Image.color = new Color(0.7f, 0.7f, 0.7f, 1f);
+                pig2Image.color = new Color(0.7f, 0.7f, 0.7f, 1f);
+                pig3Image.color = new Color(1f, 1f, 1f, 1f);
                 break;
         }
     }
@@ -45,16 +57,20 @@ public class ChooseController : MonoBehaviour
     public void ChoosePig1()
     {
         choosedPig.pigType = PigType.Pig1;
+        UpdateChooseColor();
+        
     }
     
     public void ChoosePig2()
     {
         choosedPig.pigType = PigType.Pig2;
+        UpdateChooseColor();
     }
     
     public void ChoosePig3()
     {
         choosedPig.pigType = PigType.Pig3;
+        UpdateChooseColor();
     }
     public void TrangeToGame()
     {
