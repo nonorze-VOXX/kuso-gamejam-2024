eb6eed315017154df5b88da2974634ddea55f098
Dave <Dave@buho.game.tw>
2024-04-20 16:57:12 +0800

    add skill function

--- PigTail/Assets/Scripts/ComboManager.cs
+++ PigTail/Assets/Scripts/ComboManager.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 public class ComboManager : MonoBehaviour {
     [SerializeField]Skill currentSkill;
-    Queue<comboKey> comboKeys = new Queue<comboKey>();
+    
     int currentIndex;
     private void Awake() {
         currentIndex = 0;
@@ -14,8 +14,28 @@ public class ComboManager : MonoBehaviour {
     private void Start() {
     }
     public void OnKey(comboKey comboKey){
-        
-        comboKeys.Enqueue(comboKey);
         Debug.Log(comboKey);
+        if(currentIndex>currentSkill.GetComboKeys.Length )
+            Debug.Log("ComoboKey out of range");
+        if(currentSkill.GetComboKeys[currentIndex] == comboKey){
+            currentIndex++;
+            if(currentIndex>=currentSkill.GetComboKeys.Length){
+                OnSkillActivate();
+            }
+            
+        }
+        else{
+            currentIndex = 0;
+            OnSkillWrong();
+        }
+    }
+    void OnSkillWrong(){
+        //todo: reset skill
+    }
+    void OnSkillActivate(){
+        currentIndex = 0;
+        //todo: on skill activate
+        Debug.Log("On skill!!!!!");
+
     }
 }
