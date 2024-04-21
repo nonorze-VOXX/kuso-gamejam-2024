6031517cb4ad694c3635a42c88827d800ea4c0e6
green-Desktop <appieosugood@gmail.com>
2024-04-21 14:46:46 +0800

    add new thing for popup

--- PigTail/Assets/Scripts/ComboManager.cs
+++ PigTail/Assets/Scripts/ComboManager.cs
@@ -12,6 +12,7 @@ public class ComboManager : MonoBehaviour {
     [SerializeField]EventManager eventManager;
     string comboKeys = "";
     Queue<comboKey> comboKeysQueue = new Queue<comboKey>();
+    public List<PopUpContext> popUpContexts;
 
     int currentIndex;
     float currentTime;
---
+++ PigTail/Assets/itemChoose/PopUpContext.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace itemChoose
+{
+    [CreateAssetMenu(fileName = "popUpContext", menuName = "PopupContext", order = 0)]
+    public class PopUpContext : ScriptableObject
+    {
+        public GameObject sprite;
+        public GameObject icon;
+        public string text;
+        
+    }
+}
