d7116a2bba8c62cf0c12daf234211ae9c39ebe7a
green-Desktop <appieosugood@gmail.com>
2024-04-21 12:28:18 +0800

    add random text

---
+++ PigTail/Assets/randomDialog/RandomDialog.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace randomDialog
+{
+    public class RandomDialog : MonoBehaviour
+    {
+        public TextListObject textListObject;
+        public Text dialogBox;
+        private float timer=0;
+        public float timeBetweenDialog = 5;
+        public float dialogShowTime = 5;
+        
+        private void Start()
+        {
+            dialogBox.gameObject.transform.parent.gameObject.SetActive(false);
+            timer=0;
+        }
+
+        private void Update()
+        {
+            if(dialogBox.gameObject.transform.parent.gameObject.activeSelf)
+            {
+                timer+=Time.deltaTime;
+                if (timer > dialogShowTime)
+                {
+                    dialogBox.gameObject.transform.parent.gameObject.SetActive(false);
+                    timer = 0;
+                }
+            }
+            else
+            {
+                timer+=Time.deltaTime;
+                if (timer > timeBetweenDialog)
+                {
+                    dialogBox.gameObject.transform.parent.gameObject.SetActive(true);
+                    dialogBox.text = textListObject.textList[UnityEngine.Random.Range(0, textListObject.textList.Count)];
+                    timer = 0;
+                }
+            }
+        }
+    }
+}
---
+++ PigTail/Assets/randomDialog/TextListObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace randomDialog
+{
+    [CreateAssetMenu(fileName = "textObj", menuName = "textObj", order = 0)]
+    public class TextListObject : ScriptableObject
+    {
+        public List<string> textList = new List<string>();
+    }
+}
